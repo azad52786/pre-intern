5e1958e031afa7c64c3b3fe723717570983f27e6
Kaji Azad Ali <arambagh53@gmail.com>
2026-02-11 13:24:05 +0530

    Change navbar collapse animation duration to slow for smoother transitions

--- css-advance/assignment-2/index.js
+++ css-advance/assignment-2/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.navbar__toggler').click(function() {
-        $('.navbar__collapse').slideToggle(1000);
+        $('.navbar__collapse').slideToggle("slow");
         $('.navbar__toggler').toggleClass('active');
         if($('.navbar__toggler').attr('aria-expanded') === 'true') {
             $('.navbar__toggler').attr('aria-expanded', 'false');
