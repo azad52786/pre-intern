d816c156c5635f3723f8b6519b24deb69dfeeb03
Kaji Azad Ali <arambagh53@gmail.com>
2026-02-17 12:46:01 +0530

    Refactor HTML structure and enhance form validation in assignment-3

--- jquery/assignment-3/index.js
+++ jquery/assignment-3/index.js
@@ -1,14 +1,13 @@
-$(document).ready(function() {
-    $('form').submit(function() {
-        // if($("#firstName").val().trim().length == 0 || $("#lastName").val().trim().length == 0 ) {
-        //     alert("Please fill all the fields");
-        // };
-
+$(document).ready(function () {
+    $('form').submit(function (e) {
+        let allValid = true;
         $('input').each((index, ele) => {
-            if($(ele).val().trim().length == 0) {
+            if ($(ele).val().trim().length == 0) {
                 alert("Please fill all the fields");
+                allValid = false;
                 return false;
             }
-        })
-    })
+        });
+        if(!allValid) return false;
+    })  
 });
