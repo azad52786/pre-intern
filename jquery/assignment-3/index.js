036560c480cf740bff21eb4036760383313215a0
Kaji Azad Ali <arambagh53@gmail.com>
2026-02-18 12:10:36 +0530

    Fix equality check in form validation for consistent behavior

--- jquery/assignment-3/index.js
+++ jquery/assignment-3/index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('form').submit(function (e) {
         let allValid = true;
         $('input').each((index, ele) => {
-            if ($(ele).val().trim().length == 0) {
+            if ($(ele).val().trim().length === 0) {
                 alert("Please fill all the fields");
                 allValid = false;
                 return false;
